bec07bf7fa93b0a0aa97d59c7f90563f7f3569d1
Michael Petter <michael@petterisbetter.com>
2016-10-25 00:11:08 -0500

    make chainable work more betterer with inheritance; ensure defaults are kept separated

--- lib/mores/chainable.rb
+++ lib/mores/chainable.rb
@@ -1,35 +1,39 @@
-module Mores::Chainable
-  def self.included(mod)
-    mod.extend ClassMethods
-  end
+module Mores
 
-  def **(chain)
-    @__chain = chain
-    self
-  end
+  module Chainable
+    def self.included(mod)
+      mod.extend ClassMethods
+    end
 
-  private
-
-  def __chain
-    @__chain ||= self.class.__send__(:__default_chain)
-  end
-
-  module ClassMethods
-    def chain(name, &block)
-      __forwarder.__send__(:define_method, name) do |*args|
-        __chain.public_send(name, *args)
-      end
-      __default_chain.define_singleton_method(name, block || proc { |*| })
+    def **(chain)
+      @__chain = chain
+      self
     end
 
     private
 
-    def __forwarder
-      @__forwarder ||= Module.new.tap { |m| include m }
+    def __chain
+      @__chain ||= self.class.__send__(:__default_chain).new
     end
 
-    def __default_chain
-      @@__default_chain ||= Object.new
+    module ClassMethods
+      def chain(name, &block)
+        __forwarder.__send__(:define_method, name) do |*args|
+          __chain.public_send(name, *args)
+        end
+        __default_chain.__send__(:define_method, name, block || proc { |*| })
+      end
+
+      private
+
+      def __forwarder
+        @__forwarder ||= superclass < Chainable ? superclass.__send__(:__forwarder) : Module.new.tap { |m| include m }
+      end
+
+      def __default_chain
+        @__default_chain ||= Class.new(superclass < Chainable ? superclass.__send__(:__default_chain) : Object)
+      end
     end
   end
+
 end
--- spec/chainable_spec.rb
+++ spec/chainable_spec.rb
@@ -76,5 +76,33 @@ describe Mores::Chainable do
     let(:solmyr) { Class.new(wizard) }
     subject { solmyr.new(mp).lightning enemy }
     include_examples 'all'
+
+    context 'with altered default' do
+      before do
+        solmyr.class_eval do
+          chain(:lightning) { |enemy| 'y u no zap?' }
+        end
+      end
+      include_examples 'all' do
+        let(:unbroken_value) { 'y u no zap?' }
+      end
+      it 'separates defaults' do
+        wizard; solmyr
+        expect(wizard.new(mp).lightning).to be_nil
+      end
+    end
+  end
+
+  context 'with divergent class' do
+    let(:warlock) {
+      Class.new.class_exec described_class do |described_class|
+        include described_class
+        chain(:lightning) { 'y u no zap?' }
+      self end
+    }
+    it 'separates defaults' do
+      warlock; wizard
+      expect(warlock.new.lightning).to eql 'y u no zap?'
+    end
   end
 end
