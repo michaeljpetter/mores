c936d9d1f5a42dc55f41434fcd3dd0ab731f2f25
Michael Petter <michael@petterisbetter.com>
2017-02-10 23:55:15 -0600

    use keyword arguments instead of options hash; restructure includes

--- lib/mores/immutable_struct.rb
+++ lib/mores/immutable_struct.rb
@@ -1,21 +1,19 @@
 module Mores
 
   class ImmutableStruct < ::Struct
-    require_relative 'immutable_struct/flyweight'
-    require_relative 'immutable_struct/strict'
-
-    DEFAULT_OPTIONS = { flyweight: true }.freeze
+    %w[
+      flyweight
+      strict
+    ].each { |file| require_relative "immutable_struct/#{file}" }
 
     undef_method :[]=
 
-    def self.new(*args, **options, &block)
-      options = DEFAULT_OPTIONS.merge options
-
+    def self.new(*args, flyweight: true, strict: false, &block)
       super(*args, &block).tap do |klass|
         klass.instance_eval do
           members.each { |x| remove_method "#{x}=" }
-          include Flyweight if options[:flyweight]
-          include Strict if options[:strict]
+          include Flyweight if flyweight
+          include Strict if strict
         end
       end
     end
