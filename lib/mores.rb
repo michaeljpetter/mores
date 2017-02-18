9300a20c194cc3be27b13d10b7471cbccdec926c
Michael Petter <michael@petterisbetter.com>
2017-02-18 01:38:48 -0600

    use autoload for types since this is more of a 'grab-bag' lib

--- lib/mores.rb
+++ lib/mores.rb
@@ -1,7 +1,10 @@
 %w[
   version
-  block_initialize
-  linked_list
-  succession
-  immutable_struct
 ].each { |file| require_relative "mores/#{file}" }
+
+[
+  [Mores,        :BlockInitialize, 'block_initialize'],
+  [Mores,        :LinkedList,      'linked_list'     ],
+  [Mores,        :Succession,      'succession'      ],
+  [Mores,        :ImmutableStruct, 'immutable_struct'],
+].each { |(mod, name, file)| mod.autoload name, "mores/#{file}" }
